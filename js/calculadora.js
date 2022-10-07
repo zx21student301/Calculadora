b894ad1c1d557c609c0b87278c3607c414c3497f
Eky <zx21student301@colegiovalledelmiro.es>
2022-10-07 11:34:01 +0200

    operacionSuma

---
+++ js/calculadora.js
@@ -0,0 +1,14 @@
+onload = iniciar
+
+let n1;
+let n2;
+
+function iniciar(){
+    n1 = document.getElementById("num1");
+    n2 = document.getElementById("num2");
+}
+
+function sumar(){
+    let res = n1 + n2;
+    salida.innerHTML = "La suma de "+n1+" + "+n2+" = "+res;
+}
